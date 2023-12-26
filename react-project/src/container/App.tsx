c9e0db0fe743c0f4b69032637aaf5bf850621d18
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-12-26 23:36:06 +0800

    react setup

---
+++ react-project/src/container/App.tsx
@@ -0,0 +1,19 @@
+import { Footer, Header } from "../Components/Layout"
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className="text-success">
+        <Header />
+        Main Component
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react-project/src/main.tsx
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './container/App.tsx'
+import './index.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+)
