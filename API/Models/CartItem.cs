9b5013e30d6d78898e54ec7872fae9c912c72a0d
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-12-26 14:47:51 +0800

    client

---
+++ API/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+	public class CartItem
+	{
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        [ForeignKey("MenuItemId")]       // one menuitem can be in many shopping cart
+        public MenuItem MenuItem { get; set; } = new();
+        public int Quantity { get; set; }
+        public int ShoppingCartId { get; set; }
+    }
+}
+
---
+++ API/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+	public class ShoppingCart
+	{
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }   // one shopping cart can have many cartitem
+
+        [NotMapped]
+        public double CartTotal { get; set; }
+        [NotMapped]
+        public string StripePaymentIntentId { get; set; }
+        [NotMapped]
+        public string ClientSecret { get; set; }
+    }
+}
+
